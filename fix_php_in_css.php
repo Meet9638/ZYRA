5b4d059571d1546c33e97b8415897ed620298711
Meet Parmar <165749926+Meet9638@users.noreply.github.com>
2026-06-04 13:49:57 +0530

    Delete fix_php_in_css.php

--- fix_php_in_css.php
+++
@@ -1,6 +0,0 @@
-<?php
-$file = __DIR__ . '/public/css/style.css';
-$css = file_get_contents($file);
-$css = preg_replace('/@php.*?@endphp;?/is', '/* BLADE PHP REMOVED */', $css);
-file_put_contents($file, $css);
-echo "Cleaned \\@php blocks from style.css\n";
